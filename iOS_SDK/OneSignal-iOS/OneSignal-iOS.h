0f128af7beea84b963488bde1c0ed98821887d27
Dan Appel <dan.appel00@gmail.com>
2016-01-19 12:45:15 -0800

    Create proper framework target

---
+++ iOS_SDK/OneSignal-iOS/OneSignal-iOS.h
@@ -0,0 +1,18 @@
+//
+//  OneSignal-iOS.h
+//  OneSignal-iOS
+//
+//  Created by Dan Appel on 1/19/16.
+//  Copyright © 2016 Hiptic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneSignal-iOS.
+FOUNDATION_EXPORT double OneSignal_iOSVersionNumber;
+
+//! Project version string for OneSignal-iOS.
+FOUNDATION_EXPORT const unsigned char OneSignal_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneSignal_iOS/PublicHeader.h>
+#import <OneSignal/OneSignal.h>
